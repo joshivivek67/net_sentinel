da467a33cf6296474f1145aea1a24616dc38b5ab
Vivek Joshi <joshi.vivek67@yahoo.com>
2026-01-11 22:45:34 +0530

    7D

--- src/capture.rs
+++ src/capture.rs
@@ -4,6 +4,7 @@ use extended_isolation_forest::Forest;
 use pcap::{Capture, Device};
 use serde::Serialize;
 use std::error::Error;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::time::Instant;
 //Find the main network interface
 
@@ -38,6 +39,50 @@ pub struct PacketFiled {
     len: u32,
     protocol: u8,
     iat: f64,
+    src_port: u16,
+    dst_port: u16,
+    src_ip: IpAddr,
+    dst_ip: IpAddr,
+}
+impl PacketFiled {
+    pub fn from_packet(packet: &pcap::Packet, iat: f64) -> Option<Self> {
+        let value = SlicedPacket::from_ethernet(&packet.data).ok()?;
+        let (protocol_byte, src_ip, dst_ip) = match value.ip {
+            Some(etherparse::InternetSlice::Ipv4(header, _)) => {
+                let src = IpAddr::V4(Ipv4Addr::from(header.source()));
+                let dst = IpAddr::V4(Ipv4Addr::from(header.destination()));
+                (header.protocol(), src, dst)
+            }
+            Some(etherparse::InternetSlice::Ipv6(header, _)) => {
+                let src = IpAddr::V6(Ipv6Addr::from(header.source()));
+                let dst = IpAddr::V6(Ipv6Addr::from(header.destination()));
+                (header.next_header(), src, dst)
+            }
+            _ => (
+                0u8,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+            ),
+        };
+        let (src_port, dst_port) = match value.transport {
+            Some(etherparse::TransportSlice::Tcp(header)) => {
+                (header.source_port(), header.destination_port())
+            }
+            Some(etherparse::TransportSlice::Udp(header)) => {
+                (header.source_port(), header.destination_port())
+            }
+            _ => (0, 0),
+        };
+        Some(Self {
+            len: packet.header.len,
+            protocol: protocol_byte,
+            iat,
+            src_port,
+            dst_port,
+            src_ip,
+            dst_ip,
+        })
+    }
 }
 
 //Open the Main Interface to capture packets
@@ -57,40 +102,28 @@ pub fn start_capture(device: Device) -> Result<(), Box<dyn Error>> {
         let iat = state.update_and_get_iat();
         println!("Captured packet: {} byte | IAT: {}", packet.header.len, iat);
 
-        match SlicedPacket::from_ethernet(&packet.data) {
-            Ok(value) => {
-                let protocol_byte = match value.ip {
-                    Some(etherparse::InternetSlice::Ipv4(header, _)) => header.protocol(),
-                    Some(etherparse::InternetSlice::Ipv6(header, _)) => header.next_header(),
-                    None => continue,
-                };
-                let packet_file = PacketFiled {
-                    len: packet.header.len,
-                    protocol: protocol_byte,
-                    iat,
-                };
-                file_writer.serialize(&packet_file)?;
-                let protocol_name = match protocol_byte {
-                    6 => "TCP",
-                    17 => "UDP",
-                    1 => "ICMP",
-                    41 => "IPv6",
-                    _ => "Other",
-                };
-                println!(
-                    "Captured packet: {} byte | Proto {}: ",
-                    packet.header.len, protocol_name
-                );
-            }
-            Err(e) => {
-                println!("Error parsing packet: {}", e);
-            }
+        // Just one line to parse everything!
+        if let Some(packet_file) = PacketFiled::from_packet(&packet, iat) {
+            // In start_capture:
+            file_writer.serialize(&packet_file)?;
+
+            let protocol_name = match packet_file.protocol {
+                6 => "TCP",
+                17 => "UDP",
+                1 => "ICMP",
+                41 => "IPv6",
+                _ => "Other",
+            };
+            println!(
+                "Captured packet: {} byte | Proto {}: ",
+                packet.header.len, protocol_name
+            );
         }
     }
     Ok(())
 }
 // The Detection Loop
-pub fn start_guard(device: Device, model: &Forest<f64, 3>) -> Result<(), Box<dyn Error>> {
+pub fn start_guard(device: Device, model: &Forest<f64, 7>) -> Result<(), Box<dyn Error>> {
     let mut state = CaptureState::new();
     let mut capture = Capture::from_device(device)?
         .promisc(true)
@@ -99,22 +132,31 @@ pub fn start_guard(device: Device, model: &Forest<f64, 3>) -> Result<(), Box<dyn
     println!("🛡️ GUARD ACTIVE. Monitoring traffic...");
     while let Ok(packet) = capture.next_packet() {
         let iat = state.update_and_get_iat();
-        match SlicedPacket::from_ethernet(&packet.data) {
-            Ok(value) => {
-                let protocol_byte = match value.ip {
-                    Some(etherparse::InternetSlice::Ipv4(header, _)) => header.protocol(),
-                    Some(etherparse::InternetSlice::Ipv6(header, _)) => header.next_header(),
-                    None => continue,
-                };
-                // FEATURE EXTRACTION
-                let len = packet.header.len as f64;
-                let proto = protocol_byte as f64;
-                // ASK THE BRAIN
-                if ml::is_anomaly(model, len, proto, iat) {
-                    println!("🚨 ANOMALY DETECTED! Len: {}, Proto: {}", len, proto);
-                }
+        if let Some(pf) = PacketFiled::from_packet(&packet, iat) {
+            let src_ip_num = match pf.src_ip {
+                IpAddr::V4(v4) => u32::from(v4) as f64,
+                IpAddr::V6(_) => 0.0,
+            };
+
+            let dst_ip_num = match pf.dst_ip {
+                IpAddr::V4(v4) => u32::from(v4) as f64,
+                IpAddr::V6(_) => 0.0,
+            };
+            if ml::is_anomaly(
+                model,
+                pf.len as f64,
+                pf.protocol as f64,
+                iat,
+                pf.src_port as f64,
+                pf.dst_port as f64,
+                src_ip_num,
+                dst_ip_num,
+            ) {
+                println!(
+                    "🚨 ANOMALY DETECTED! Len: {}, Proto: {}, IAT: {}, Src Port: {}, Dst Port: {}, Src IP: {}, Dst IP: {}",
+                    pf.len, pf.protocol, iat, pf.src_port, pf.dst_port, src_ip_num, dst_ip_num
+                );
             }
-            Err(_) => {}
         }
     }
     Ok(())
--- src/ml.rs
+++ src/ml.rs
@@ -3,8 +3,9 @@ use log::info;
 use std::error::Error;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::net::IpAddr;
 // Define our data type: A list of rows, where each row is a list of numbers
-pub type Dataset = Vec<[f64; 3]>;
+pub type Dataset = Vec<[f64; 7]>;
 
 pub fn load_training_data() -> Result<Dataset, Box<dyn Error>> {
     let mut reader = csv::Reader::from_path("training.data.csv")?;
@@ -17,7 +18,14 @@ pub fn load_training_data() -> Result<Dataset, Box<dyn Error>> {
         };
 
         // SAFETY CHECK: Ensure we actually have 2 columns and they aren't empty
-        if record.len() < 3 || record[0].is_empty() || record[1].is_empty() || record[2].is_empty()
+        if record.len() < 3
+            || record[0].is_empty()
+            || record[1].is_empty()
+            || record[2].is_empty()
+            || record[3].is_empty()
+            || record[4].is_empty()
+            || record[5].is_empty()
+            || record[6].is_empty()
         {
             continue;
         }
@@ -35,13 +43,35 @@ pub fn load_training_data() -> Result<Dataset, Box<dyn Error>> {
             Ok(n) => n,
             Err(_) => continue,
         };
+        let src_port: f64 = match record[3].parse() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+        let dst_port: f64 = match record[4].parse() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+        let src_ip_num: f64 = record[5]
+            .parse::<IpAddr>()
+            .map(|ip| match ip {
+                IpAddr::V4(v4) => u32::from(v4) as f64,
+                IpAddr::V6(_) => 0.0,
+            })
+            .unwrap_or(0.0);
+        let dst_ip_num: f64 = record[6]
+            .parse::<IpAddr>()
+            .map(|ip| match ip {
+                IpAddr::V4(v4) => u32::from(v4) as f64,
+                IpAddr::V6(_) => 0.0,
+            })
+            .unwrap_or(0.0);
 
-        data.push([len, proto, iat]);
+        data.push([len, proto, iat, src_port, dst_port, src_ip_num, dst_ip_num]);
     }
     Ok(data)
 }
 
-pub fn train_model(data: &Dataset) -> Result<Forest<f64, 3>, Box<dyn Error>> {
+pub fn train_model(data: &Dataset) -> Result<Forest<f64, 7>, Box<dyn Error>> {
     info!(
         "Training Extended Isolation Forest on {} packets...",
         data.len()
@@ -63,37 +93,46 @@ pub fn train_model(data: &Dataset) -> Result<Forest<f64, 3>, Box<dyn Error>> {
     Ok(forest)
 }
 
-pub fn save_model(model: &Forest<f64, 3>) -> Result<(), Box<dyn Error>> {
+pub fn save_model(model: &Forest<f64, 7>) -> Result<(), Box<dyn Error>> {
     let mut file = File::create("model_isolation_forest.json")?;
-    let serialized = serde_json::to_string(model)?;
+    let serialized = serde_json::to_string_pretty(model)?;
     file.write_all(serialized.as_bytes())?;
     info!("Model saved successfully!");
     Ok(())
 }
-pub fn load_model(path: &str) -> Result<Forest<f64, 3>, Box<dyn Error>> {
+pub fn load_model(path: &str) -> Result<Forest<f64, 7>, Box<dyn Error>> {
     let mut file = File::open(path)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
 
     // Deserialize the JSON string back into a Forest
-    let model: Forest<f64, 3> = serde_json::from_str(&content)?;
+    let model: Forest<f64, 7> = serde_json::from_str(&content)?;
     Ok(model)
 }
 
-pub fn is_anomaly(model: &Forest<f64, 3>, len: f64, proto: f64, iat: f64) -> bool {
-    let point = [len, proto, iat];
+pub fn is_anomaly(
+    model: &Forest<f64, 7>,
+    len: f64,
+    proto: f64,
+    iat: f64,
+    src_port: f64,
+    dst_port: f64,
+    src_ip_num: f64,
+    dst_ip_num: f64,
+) -> bool {
+    let point = [len, proto, iat, src_port, dst_port, src_ip_num, dst_ip_num];
     let score = model.score(&point);
 
     // DEBUG: Print EVERY score so we can see what is happening!
-    info!(
-        "Packet Score: {:.4} (Len: {}, Proto: {}, IAT: {})",
-        score, len, proto, iat
-    );
+    //info!(
+    //    "Packet Score: {:.4} (Len: {}, Proto: {}, IAT: {} ,Src Port: {}, Dst Port: {}, Src IP: {}, Dst IP: {})",
+    //    score, len, proto, iat, src_port, dst_port, src_ip_num, dst_ip_num
+    //);
 
     if score > 0.6 {
         info!(
-            "🚨 ANOMALY! Score: {:.4} (Len: {}, Proto: {}, IAT: {})",
-            score, len, proto, iat
+            "🚨 ANOMALY! Score: {:.4} (Len: {}, Proto: {}, IAT: {} ,Src Port: {}, Dst Port: {}, Src IP: {}, Dst IP: {})",
+            score, len, proto, iat, src_port, dst_port, src_ip_num, dst_ip_num
         );
         return true;
     }
